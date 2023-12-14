16f39d9b26980d1a701738ed1ccbac8686c16575
AdamUAT <115857693+AdamUAT@users.noreply.github.com>
2023-12-14 13:59:46 -0700

    Made free chess pieces

--- Chess/Forms/Board.Designer.cs
+++ Chess/Forms/Board.Designer.cs
@@ -40,15 +40,16 @@
             // 
             ContentPanel.Controls.Add(BoardImage);
             ContentPanel.Controls.Add(BoardBackground);
-            ContentPanel.Location = new Point(0, 0);
-            ContentPanel.Size = new Size(800, 450);
+            ContentPanel.Location = new Point(50, 0);
+            ContentPanel.Size = new Size(600, 338);
             // 
             // BoardBackground
             // 
             BoardBackground.Image = Properties.Resources.Wood1;
             BoardBackground.Location = new Point(0, 0);
+            BoardBackground.Margin = new Padding(3, 2, 3, 2);
             BoardBackground.Name = "BoardBackground";
-            BoardBackground.Size = new Size(800, 450);
+            BoardBackground.Size = new Size(700, 338);
             BoardBackground.SizeMode = PictureBoxSizeMode.StretchImage;
             BoardBackground.TabIndex = 0;
             BoardBackground.TabStop = false;
@@ -56,18 +57,19 @@
             // BoardImage
             // 
             BoardImage.Image = Properties.Resources.Board;
-            BoardImage.Location = new Point(185, 10);
+            BoardImage.Location = new Point(162, 8);
+            BoardImage.Margin = new Padding(3, 2, 3, 2);
             BoardImage.Name = "BoardImage";
-            BoardImage.Size = new Size(430, 430);
+            BoardImage.Size = new Size(376, 322);
             BoardImage.SizeMode = PictureBoxSizeMode.Zoom;
             BoardImage.TabIndex = 1;
             BoardImage.TabStop = false;
             // 
             // Board
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(700, 338);
             Name = "Board";
             Text = "Board";
             ContentPanel.ResumeLayout(false);
--- Chess/Forms/Board.cs
+++ Chess/Forms/Board.cs
@@ -14,17 +14,108 @@ namespace Chess
     {
         float boardMarginPercent = 0;
 
-        BoardSpace[] spaces = new BoardSpace[64];
+        List<Piece> pieces = new List<Piece>();
 
         public Board()
         {
             InitializeComponent();
 
             //Initialize the board
-            for (int i = 0; i < 64; i++)
+            Piece leftBlackRook = new Piece(Piece.PieceTypes.Rook, true, BoardImage);
+            leftBlackRook.SetLocation((int)(BoardImage.Width * 1 / 18.0f), (int)(BoardImage.Height * 1 / 18.0f));
+            leftBlackRook.SetScale((int)(BoardImage.Width * 1.0f / 9), (int)(BoardImage.Height * 1.0f / 9));
+            pieces.Add(leftBlackRook);
+
+            Piece leftBlackKnight = new Piece(Piece.PieceTypes.Knight, true, BoardImage);
+            leftBlackKnight.SetLocation((int)(BoardImage.Width * 3 / 18.0f), (int)(BoardImage.Height * 1 / 18.0f));
+            leftBlackKnight.SetScale((int)(BoardImage.Width * 1.0f / 9), (int)(BoardImage.Height * 1.0f / 9));
+            pieces.Add(leftBlackKnight);
+
+            Piece leftBlackBishop = new Piece(Piece.PieceTypes.Bishop, true, BoardImage);
+            leftBlackBishop.SetLocation((int)(BoardImage.Width * 5 / 18.0f), (int)(BoardImage.Height * 1 / 18.0f));
+            leftBlackBishop.SetScale((int)(BoardImage.Width * 1.0f / 9), (int)(BoardImage.Height * 1.0f / 9));
+            pieces.Add(leftBlackBishop);
+
+            Piece blackKing = new Piece(Piece.PieceTypes.King, true, BoardImage);
+            blackKing.SetLocation((int)(BoardImage.Width * 7 / 18.0f), (int)(BoardImage.Height * 1 / 18.0f));
+            blackKing.SetScale((int)(BoardImage.Width * 1.0f / 9), (int)(BoardImage.Height * 1.0f / 9));
+            pieces.Add(blackKing);
+
+            Piece blackQueen = new Piece(Piece.PieceTypes.Queen, true, BoardImage);
+            blackQueen.SetLocation((int)(BoardImage.Width * 9 / 18.0f), (int)(BoardImage.Height * 1 / 18.0f));
+            blackQueen.SetScale((int)(BoardImage.Width * 1.0f / 9), (int)(BoardImage.Height * 1.0f / 9));
+            pieces.Add(blackQueen);
+
+            Piece rightBlackBishop = new Piece(Piece.PieceTypes.Bishop, true, BoardImage);
+            rightBlackBishop.SetLocation((int)(BoardImage.Width * 11 / 18.0f), (int)(BoardImage.Height * 1 / 18.0f));
+            rightBlackBishop.SetScale((int)(BoardImage.Width * 1.0f / 9), (int)(BoardImage.Height * 1.0f / 9));
+            pieces.Add(rightBlackBishop);
+
+            Piece rightBlackKnight = new Piece(Piece.PieceTypes.Knight, true, BoardImage);
+            rightBlackKnight.SetLocation((int)(BoardImage.Width * 13 / 18.0f), (int)(BoardImage.Height * 1 / 18.0f));
+            rightBlackKnight.SetScale((int)(BoardImage.Width * 1.0f / 9), (int)(BoardImage.Height * 1.0f / 9));
+            pieces.Add(rightBlackKnight);
+
+            Piece rightBlackRook = new Piece(Piece.PieceTypes.Rook, true, BoardImage);
+            rightBlackRook.SetLocation((int)(BoardImage.Width * 15 / 18.0f), (int)(BoardImage.Height * 1 / 18.0f));
+            rightBlackRook.SetScale((int)(BoardImage.Width * 1.0f / 9), (int)(BoardImage.Height * 1.0f / 9));
+            pieces.Add(rightBlackRook);
+
+            for(int i = 0; i < 8; i++)
             {
-                spaces[i] = new BoardSpace(Piece.PieceTypes.Pawn, true, BoardImage);
+                Piece blackPawn = new Piece(Piece.PieceTypes.Pawn, true, BoardImage);
+                blackPawn.SetLocation((int)(BoardImage.Width * (i * 2 + 1) / 18.0f), (int)(BoardImage.Height * 3 / 18.0f));
+                blackPawn.SetScale((int)(BoardImage.Width * 1.0f / 9), (int)(BoardImage.Height * 1.0f / 9));
+                pieces.Add(blackPawn);
             }
+
+            for (int i = 0; i < 8; i++)
+            {
+                Piece blackPawn = new Piece(Piece.PieceTypes.Pawn, false, BoardImage);
+                blackPawn.SetLocation((int)(BoardImage.Width * (i * 2 + 1) / 18.0f), (int)(BoardImage.Height * 13 / 18.0f));
+                blackPawn.SetScale((int)(BoardImage.Width * 1.0f / 9), (int)(BoardImage.Height * 1.0f / 9));
+                pieces.Add(blackPawn);
+            }
+
+            Piece leftWhiteRook = new Piece(Piece.PieceTypes.Rook, false, BoardImage);
+            leftWhiteRook.SetLocation((int)(BoardImage.Width * 1 / 18.0f), (int)(BoardImage.Height * 15 / 18.0f));
+            leftWhiteRook.SetScale((int)(BoardImage.Width * 1.0f / 9), (int)(BoardImage.Height * 1.0f / 9));
+            pieces.Add(leftWhiteRook);
+
+            Piece leftWhiteKnight = new Piece(Piece.PieceTypes.Knight, false, BoardImage);
+            leftWhiteKnight.SetLocation((int)(BoardImage.Width * 3 / 18.0f), (int)(BoardImage.Height * 15 / 18.0f));
+            leftWhiteKnight.SetScale((int)(BoardImage.Width * 1.0f / 9), (int)(BoardImage.Height * 1.0f / 9));
+            pieces.Add(leftWhiteKnight);
+
+            Piece leftWhiteBishop = new Piece(Piece.PieceTypes.Bishop, false, BoardImage);
+            leftWhiteBishop.SetLocation((int)(BoardImage.Width * 5 / 18.0f), (int)(BoardImage.Height * 15 / 18.0f));
+            leftWhiteBishop.SetScale((int)(BoardImage.Width * 1.0f / 9), (int)(BoardImage.Height * 1.0f / 9));
+            pieces.Add(leftWhiteBishop);
+
+            Piece whiteKing = new Piece(Piece.PieceTypes.King, false, BoardImage);
+            whiteKing.SetLocation((int)(BoardImage.Width * 7 / 18.0f), (int)(BoardImage.Height * 15 / 18.0f));
+            whiteKing.SetScale((int)(BoardImage.Width * 1.0f / 9), (int)(BoardImage.Height * 1.0f / 9));
+            pieces.Add(whiteKing);
+
+            Piece whiteQueen = new Piece(Piece.PieceTypes.Queen, false, BoardImage);
+            whiteQueen.SetLocation((int)(BoardImage.Width * 9 / 18.0f), (int)(BoardImage.Height * 15 / 18.0f));
+            whiteQueen.SetScale((int)(BoardImage.Width * 1.0f / 9), (int)(BoardImage.Height * 1.0f / 9));
+            pieces.Add(whiteQueen);
+
+            Piece rightWhiteBishop = new Piece(Piece.PieceTypes.Bishop, false, BoardImage);
+            rightWhiteBishop.SetLocation((int)(BoardImage.Width * 11 / 18.0f), (int)(BoardImage.Height * 15 / 18.0f));
+            rightWhiteBishop.SetScale((int)(BoardImage.Width * 1.0f / 9), (int)(BoardImage.Height * 1.0f / 9));
+            pieces.Add(rightWhiteBishop);
+
+            Piece rightWhiteKnight = new Piece(Piece.PieceTypes.Knight, false, BoardImage);
+            rightWhiteKnight.SetLocation((int)(BoardImage.Width * 13 / 18.0f), (int)(BoardImage.Height * 15 / 18.0f));
+            rightWhiteKnight.SetScale((int)(BoardImage.Width * 1.0f / 9), (int)(BoardImage.Height * 1.0f / 9));
+            pieces.Add(rightWhiteKnight);
+
+            Piece rightWhiteRook = new Piece(Piece.PieceTypes.Rook, false, BoardImage);
+            rightWhiteRook.SetLocation((int)(BoardImage.Width * 15 / 18.0f), (int)(BoardImage.Height * 15 / 18.0f));
+            rightWhiteRook.SetScale((int)(BoardImage.Width * 1.0f / 9), (int)(BoardImage.Height * 1.0f / 9));
+            pieces.Add(rightWhiteRook);
         }
 
         protected override void OnResize()
@@ -41,10 +132,22 @@ namespace Chess
 
             int boardMargin = (int)(boardMarginPercent * BoardBackground.Height);
 
-            BoardImage.Height = BoardBackground.Height - boardMargin * 2;
-            BoardImage.Width = BoardImage.Height;
-            BoardImage.Location = new Point((int)((BoardBackground.Width / 2.0f) - (BoardImage.Width / 2.0f)), boardMargin);
+            int boardImageHeight = BoardBackground.Height - boardMargin * 2;
+            int boardImageWidth = BoardImage.Height;
+            Point boardImageLocation = new Point((int)((BoardBackground.Width / 2.0f) - (BoardImage.Width / 2.0f)), boardMargin);
 
+            foreach(Piece piece in  pieces)
+            {
+                float locationPercentX = (float)piece.GetLocation().X / BoardImage.Width;
+                float locationPercentY = (float)piece.GetLocation().Y / BoardImage.Height;
+                piece.SetLocation((int)(locationPercentX * boardImageWidth), (int)(locationPercentY * boardImageHeight));
+
+                piece.SetScale((int)(boardImageWidth * 1.0f / 9), (int)(boardImageHeight * 1.0f / 9));
+            }
+
+            BoardImage.Height = boardImageHeight;
+            BoardImage.Width = boardImageWidth;
+            BoardImage.Location = boardImageLocation;
         }
 
 
@@ -172,7 +275,8 @@ namespace Chess
 
         private void DragPiece(Object sender, EventArgs e)
         {
-            image.Location = parent.PointToClient(Control.MousePosition);
+            Point mousePoint = parent.PointToClient(Control.MousePosition);
+            image.Location = new Point((int)(mousePoint.X - image.Width / 2.0f), (int)(mousePoint.Y - image.Height / 2.0f));
 
         }
 
@@ -181,6 +285,28 @@ namespace Chess
             image.Click += StartDragging;
             image.Click -= StopDragging;
             Program.updateTimer.Tick -= DragPiece;
+        }
+
+        public void SetLocation(int x, int y)
+        {
+            image.Location = new Point(x, y);
+        }
+
+        public void SetScale(int width, int height)
+        {
+            image.Width = width;
+            image.Height = height;
+        }
+
+        public Point GetLocation()
+        {
+            return(image.Location);
+        }
+
+        //The pieces should be square, so the width and height are the same.
+        public int GetScale()
+        {
+            return (image.Width);
         }
     }
 }
