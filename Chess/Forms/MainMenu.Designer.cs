dde35177f3664839923d747beeeda95868423250
AdamUAT <115857693+AdamUAT@users.noreply.github.com>
2023-12-14 03:47:25 -0700

    Created buttons in main menu

--- Chess/Forms/MainMenu.Designer.cs
+++ Chess/Forms/MainMenu.Designer.cs
@@ -2,7 +2,7 @@
 
 namespace Chess
 {
-    partial class MainMenu 
+    partial class MainMenu
     {
         /// <summary>
         /// Required designer variable.
@@ -30,12 +30,141 @@ namespace Chess
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainMenu";
+            MainMenuBackground = new PictureBox();
+            PlayButton = new PictureBox();
+            SettingsButton = new PictureBox();
+            QuitButton = new PictureBox();
+            PlayButtonText = new PictureBox();
+            SettingsButtonText = new PictureBox();
+            QuitButtonText = new PictureBox();
+            ContentPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)MainMenuBackground).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)PlayButton).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)SettingsButton).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)QuitButton).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)PlayButtonText).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)SettingsButtonText).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)QuitButtonText).BeginInit();
+            SuspendLayout();
+            // 
+            // ContentPanel
+            // 
+            
+            ContentPanel.Controls.Add(MainMenuBackground);
+            ContentPanel.Location = new Point(0, 0);
+            ContentPanel.Size = new Size(800, 450);
+            // 
+            // MainMenuBackground
+            // 
+            QuitButton.Controls.Add(QuitButtonText);
+            MainMenuBackground.Controls.Add(QuitButton);
+            MainMenuBackground.Controls.Add(SettingsButton);
+            MainMenuBackground.Controls.Add(PlayButton);
+            MainMenuBackground.Dock = DockStyle.Fill;
+            MainMenuBackground.Image = Properties.Resources.MainMenuScreen;
+            MainMenuBackground.Location = new Point(0, 0);
+            MainMenuBackground.Name = "MainMenuBackground";
+            MainMenuBackground.Size = new Size(800, 450);
+            MainMenuBackground.SizeMode = PictureBoxSizeMode.StretchImage;
+            MainMenuBackground.TabIndex = 0;
+            MainMenuBackground.TabStop = false;
+            // 
+            // PlayButton
+            //
+            PlayButton.Controls.Add(PlayButtonText);
+            PlayButton.BackColor = Color.Transparent;
+            PlayButton.Image = Properties.Resources.MainMenuButton;
+            PlayButton.Location = new Point(132, 170);
+            PlayButton.Name = "PlayButton";
+            PlayButton.Size = new Size(315, 81);
+            PlayButton.SizeMode = PictureBoxSizeMode.Zoom;
+            PlayButton.TabIndex = 1;
+            PlayButton.TabStop = false;
+            PlayButton.Click += PlayButton_Click;
+            // 
+            // SettingsButton
+            // 
+            SettingsButton.Controls.Add(SettingsButtonText);
+            SettingsButton.BackColor = Color.Transparent;
+            SettingsButton.Image = Properties.Resources.MainMenuButton;
+            SettingsButton.Location = new Point(96, 248);
+            SettingsButton.Name = "SettingsButton";
+            SettingsButton.Size = new Size(315, 81);
+            SettingsButton.SizeMode = PictureBoxSizeMode.Zoom;
+            SettingsButton.TabIndex = 2;
+            SettingsButton.TabStop = false;
+            // 
+            // QuitButton
+            // 
+            QuitButton.Controls.Add(QuitButtonText);
+            QuitButton.BackColor = Color.Transparent;
+            QuitButton.Image = Properties.Resources.MainMenuButton;
+            QuitButton.Location = new Point(62, 322);
+            QuitButton.Name = "QuitButton";
+            QuitButton.Size = new Size(315, 81);
+            QuitButton.SizeMode = PictureBoxSizeMode.Zoom;
+            QuitButton.TabIndex = 3;
+            QuitButton.TabStop = false;
+            // 
+            // PlayButtonText
+            // 
+            PlayButtonText.BackColor = Color.Transparent;
+            PlayButtonText.Image = Properties.Resources.PlayText;
+            PlayButtonText.Location = new Point(21, 21);
+            PlayButtonText.Name = "PlayButtonText";
+            PlayButtonText.Size = new Size(103, 40);
+            PlayButtonText.SizeMode = PictureBoxSizeMode.Zoom;
+            PlayButtonText.TabIndex = 4;
+            PlayButtonText.TabStop = false;
+            // 
+            // SettingsButtonText
+            // 
+            SettingsButtonText.BackColor = Color.Transparent;
+            SettingsButtonText.Image = Properties.Resources.SettingsText;
+            SettingsButtonText.Location = new Point(21, 21);
+            SettingsButtonText.Name = "SettingsButtonText";
+            SettingsButtonText.Size = new Size(164, 40);
+            SettingsButtonText.SizeMode = PictureBoxSizeMode.Zoom;
+            SettingsButtonText.TabIndex = 5;
+            SettingsButtonText.TabStop = false;
+            // 
+            // QuitButtonText
+            // 
+            QuitButtonText.BackColor = Color.Transparent;
+            QuitButtonText.Image = Properties.Resources.QuitText;
+            QuitButtonText.Location = new Point(21, 21);
+            QuitButtonText.Name = "QuitButtonText";
+            QuitButtonText.Size = new Size(125, 40);
+            QuitButtonText.SizeMode = PictureBoxSizeMode.Zoom;
+            QuitButtonText.TabIndex = 6;
+            QuitButtonText.TabStop = false;
+            // 
+            // MainMenu
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "MainMenu";
+            Text = "MainMenu";
+            ContentPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)MainMenuBackground).EndInit();
+            ((System.ComponentModel.ISupportInitialize)PlayButton).EndInit();
+            ((System.ComponentModel.ISupportInitialize)SettingsButton).EndInit();
+            ((System.ComponentModel.ISupportInitialize)QuitButton).EndInit();
+            ((System.ComponentModel.ISupportInitialize)PlayButtonText).EndInit();
+            ((System.ComponentModel.ISupportInitialize)SettingsButtonText).EndInit();
+            ((System.ComponentModel.ISupportInitialize)QuitButtonText).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox MainMenuBackground;
+        private PictureBox PlayButton;
+        private PictureBox QuitButton;
+        private PictureBox SettingsButton;
+        private PictureBox PlayButtonText;
+        private PictureBox QuitButtonText;
+        private PictureBox SettingsButtonText;
     }
 }
--- Chess/Forms/MainMenu.cs
+++ Chess/Forms/MainMenu.cs
@@ -7,14 +7,91 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.Design;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Chess
 {
     public partial class MainMenu : AdjustableForm
     {
+        Bitmap buttonImage;
+
         public MainMenu()
         {
             InitializeComponent();
+
+            //Calculate the image of the buttons as a bitmap.
+            buttonImage = (Bitmap)PlayButton.Image;
+
+        }
+
+        protected override void OnResize()
+        {
+            base.OnResize();
+
+            MainMenuBackground.Size = new Size(this.ContentPanel.Width, this.ContentPanel.Height);
+        }
+
+        /// <summary>
+        /// Sees if the mouse is on the actual representation of a button and not just it's rect
+        /// </summary>
+        private bool IsMouseOnButton(int buttonWidth, int buttonHeight, Point mousePosition, Bitmap image)
+        {
+            Point scaledClickPoint;
+
+            if (buttonWidth / buttonHeight > image.Width / image.Height)
+            {
+                //The PictureBox is stretched widthwise
+
+                //Find the vertical scale amount
+                float scalePercent = (float)buttonHeight / image.Height;
+
+                //Find the horizontal space that should be ignored.
+                int scaledImageWidth = (int)(image.Width * scalePercent);
+                int pixelsToIgnore = buttonWidth - scaledImageWidth;
+
+                //Check that the cursor is in the bounds of the image
+                if (mousePosition.X < pixelsToIgnore / 2 || mousePosition.X > buttonWidth - pixelsToIgnore / 2)
+                {
+                    return false;
+                }
+
+                //Unscale the click point
+                scaledClickPoint = new Point((int)((mousePosition.X - (pixelsToIgnore / 2)) / scalePercent), (int)(mousePosition.Y / scalePercent));
+            }
+            else
+            {
+                //The PictureBox is stretched heightwise
+
+                //Find the horizontal scale amount
+                float scalePercent = (float)buttonWidth / image.Width;
+
+                //Find the vertical space that should be ignored.
+                int scaledImageHeight = (int)(image.Height * scalePercent);
+                int pixelsToIgnore = buttonHeight - scaledImageHeight;
+
+                //Check that the cursor is in the bounds of the image
+                if (mousePosition.Y < pixelsToIgnore / 2 || mousePosition.Y > buttonHeight - pixelsToIgnore / 2)
+                {
+                    return false;
+                }
+
+                //Unscale the click point
+                scaledClickPoint = new Point((int)(mousePosition.X / scalePercent), (int)((mousePosition.Y - (pixelsToIgnore / 2)) / scalePercent));
+            }
+
+            return (image.GetPixel(scaledClickPoint.X, scaledClickPoint.Y).A > 0);
+        }
+
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs args = e as MouseEventArgs;
+            Point clickPoint = new Point(args.X, args.Y);
+
+            if (IsMouseOnButton(PlayButton.Width, PlayButton.Height, clickPoint, buttonImage))
+            {
+                MessageBox.Show("Success!");
+            }
         }
     }
 }
