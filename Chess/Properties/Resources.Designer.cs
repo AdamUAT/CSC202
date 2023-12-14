c45b8ef02019db71a296dc9436a3dd441bfe3e37
AdamUAT <115857693+AdamUAT@users.noreply.github.com>
2023-12-14 09:09:59 -0700

    Created board image

--- Chess/Properties/Resources.Designer.cs
+++ Chess/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Chess.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Board {
+            get {
+                object obj = ResourceManager.GetObject("Board", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap MainMenuButton {
             get {
                 object obj = ResourceManager.GetObject("MainMenuButton", resourceCulture);
